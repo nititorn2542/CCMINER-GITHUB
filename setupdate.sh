0e021db0086a513a15da61dfd5c66affafd85870
nititorn2542 <56989005+nititorn2542@users.noreply.github.com>
2024-10-22 17:59:16 +0700

    Update setupdate.sh

--- setupdate.sh
+++ setupdate.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 apt-get update -y
-apt-get upgrade -y
 apt-get install libcurl4-openssl-dev -y
 apt-get install libssl-dev -y
 apt-get install libjansson-dev -y
@@ -13,7 +12,7 @@ apt-get install nano -y
 
 chmod +x edit-miner
 chmod +x run-miner
-chmod +x add-ip
+chmod +x add-file
 chmod +x update
 chmod +x down-grade
 chmod +x ANSI_Shadow.flf
@@ -29,10 +28,14 @@ apt-get install python3-progress -y
 apt-get install python3-requests -y
 
 
+apt-get install libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 -y
+
+apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang -y
+
 mv mobile-mining ../../etc
 mv edit-miner ../../bin
 mv run-miner ../../bin
-mv add-ip ../../bin
+mv add-file ../../bin
 mv update ../../bin
 mv down-grade ../../bin
 mv ANSI_Shadow.flf ../../usr/share/figlet
@@ -47,8 +50,9 @@ cd && cd ../etc/mobile-mining/ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
-./build.sh
+CXX=clang++ CC=clang ./build.sh
 
 chmod +x ccminer
+cd && apt-get remove libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lld-16 libclc-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang -y && apt-get clean
 
 run-miner
